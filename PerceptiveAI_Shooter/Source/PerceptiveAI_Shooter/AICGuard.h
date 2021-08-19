e2a3445079fdf0d3d8153284e776c72f25ca1e36
Dev Das <devdastour@gmail.com>
2021-08-19 13:50:05 +0530

    Added Project Content Files

---
+++ PerceptiveAI_Shooter/Source/PerceptiveAI_Shooter/AICGuard.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AICGuard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PERCEPTIVEAI_SHOOTER_API AAICGuard : public AAIController
+{
+	GENERATED_BODY()
+	
+};
---
+++ PerceptiveAI_Shooter/Source/PerceptiveAI_Shooter/PerceptiveAI_Shooter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PerceptiveAI_Shooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PerceptiveAI_Shooter, "PerceptiveAI_Shooter" );
