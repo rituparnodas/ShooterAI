06b0b250381f4d285e263298a189d13d67b49921
Dev Das <devdastour@gmail.com>
2021-08-29 23:06:07 +0530

    Working On Behaviour Tree

---
+++ PerceptiveAI_Shooter/Source/PerceptiveAI_Shooter/AI/BTT_SetAimingMode.cpp
@@ -0,0 +1,5 @@
+// Copyright DevDasTour 2021 All Right Reserved.
+
+
+#include "PerceptiveAI_Shooter/AI/BTT_SetAimingMode.h"
+
---
+++ PerceptiveAI_Shooter/Source/PerceptiveAI_Shooter/AI/BTT_SetAimingMode.h
@@ -0,0 +1,17 @@
+// Copyright DevDasTour 2021 All Right Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTT_SetAimingMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PERCEPTIVEAI_SHOOTER_API UBTT_SetAimingMode : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
